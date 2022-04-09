747e9651e7b76871b44c4179982f124950491071
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-04-09 14:39:20 -0600

    Update EventManager.java

--- src/main/java/dev/based/vampyrix/managers/EventManager.java
+++ src/main/java/dev/based/vampyrix/managers/EventManager.java
@@ -10,7 +10,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 
 public class EventManager implements Wrapper {
-
     public EventManager() {
     	MinecraftForge.EVENT_BUS.register(this);
         this.getVampyrix().getEventBus().register(this);
@@ -18,14 +17,14 @@ public class EventManager implements Wrapper {
     
     @SubscribeEvent
     public void onLivingUpdate(LivingEvent.LivingUpdateEvent event) {
-        if(!nullCheck() && event.getEntity() == mc.player) {
+        if(!this.nullCheck() && event.getEntity() == mc.player) {
             this.getVampyrix().getModuleManager().getModules().stream().filter(Module::isEnabled).forEach(Module::onUpdate);
         }
     }
     
     @SubscribeEvent
 	public void onKey(InputEvent.KeyInputEvent event) {
-		if(nullCheck() && Keyboard.getEventKeyState() && Keyboard.getEventKey() > 1) {
+		if(!this.nullCheck() && Keyboard.getEventKeyState() && Keyboard.getEventKey() > 1) {
             this.getVampyrix().getModuleManager().getModules().stream().filter(module -> module.getKeybind().getValue().getKeyCode() == Keyboard.getEventKey()).forEach(Module::toggle);
         }
 	}
